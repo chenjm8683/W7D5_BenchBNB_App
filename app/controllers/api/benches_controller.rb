e04b2529443ef870fb8a1491934b0fbb41d9bf80
Jingming Chen <chenjm8683@gmail.com>
2015-12-10 23:48:02 -0800

    phase 0

---
+++ app/controllers/api/benches_controller.rb
@@ -0,0 +1,13 @@
+class Api::BenchesController < ApplicationController
+  def index
+    render json: Bench.all
+  end
+
+  def create
+    render json: Bench.create!(bench_params)
+  end
+
+  private
+  def bench_params
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Bench.create([
+  {
+    description: "Russian Hill's Panoramic Penthouse",
+    lat: 37.801568,
+    lng: -122.417884
+  },
+  {
+    description: "New Luxury Home w/Spa Bath & Views",
+    lat: 37.755124,
+    lng: -122.435261
+  },
+  {
+    description: "Luxury + Sweeping Views+ Downtown",
+    lat: 37.778977,
+    lng: -122.409831
+  },
+
+
+
+
+  ])
